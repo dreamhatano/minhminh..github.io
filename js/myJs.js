8066fd4876d3edb4dbed0a5a3b8a935d6ee5f96d
dreamhatano <77372301+dreamhatano@users.noreply.github.com>
2021-01-19 22:14:11 +0700

    Add files via upload

--- js/myJs.js
+++ js/myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'Cậu ơiii!',
-        text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
+        title: 'Em ơiii!',
+        text: 'Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì tớ thích cậu :<<<<<<< ";
+    var text = " Tại vì em thích anh :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi :vvvv',
+        title: 'Nói cho anh lí do em thích anh đi :vvvv',
         html: true,
         width: 900,
         padding: '3em',
@@ -114,20 +114,35 @@ $('#yes').click(function() {
         cancelButtonColor: '#d33',
         confirmButtonColor: '#fe8a71',
         cancelButtonColor: '#f6cd61',
-        confirmButtonText: 'Gửi cho tớ <3'
+        confirmButtonText: 'Gửi tới anh <3'
     }).then((result) => {
         if (result.value) {
             Swal.fire({
                 width: 900,
                 confirmButtonText: 'Okiiiii lun <3',
                 background: '#fff url("img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu 300.000 luôn',
-                text: "Tối nay tớ qua đón cậu đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
+                title: 'Anh biết mà ^^ Yêu em 300.000 luôn',
+                text: "Tối nay anh qua đón em đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                     window.location = 'http://fb.com';
                   }
             })
         }
+        else
+        {
+            Swal.fire({
+                width: 900,
+                confirmButtonText: 'Okiiiii lun <3',
+                background: '#fff url("img/iput-bg.jpg")',
+                title: 'chời ơi, ngại gì mà ngại. A Yêu em lắm lắm <333333 ',
+                text: "Tối nay anh qua đón em đi chơi nhaaaaaaaaa :v Còn giờ thì chờ gì nữa mà ko inbox cho anh đi nàoooooo",
+                confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = 'http://fb.com';
+                  }
+            })
+        }
+
     })
 })
